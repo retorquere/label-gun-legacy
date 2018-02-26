b2168450676151835e742dfd36088a74c363cd4b
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-02-26 13:15:13 +0100

    class

--- build/index.js
+++ build/index.js
@@ -7,56 +7,58 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const _ = require('lodash');
-function load(robot, context) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const request = {
-            config: null,
-            isContributor: false,
-            ignore: false,
-            reopen: false,
-            issue: null,
-            edits: null,
-            label: null,
-            unlabel: null,
-            save: null,
-        };
-        try {
-            request.config = yield context.config('config.yml');
-        }
-        catch (err) {
-            robot.log(err);
-            request.config = null;
-        }
-        request.config = ((request.config || {})['label-gun']) || {};
-        if (!request.config.labels)
-            request.config.labels = {};
-        if (!request.config.labels.feedback)
-            request.config.labels.feedback = 'awaiting-user-feedback';
-        request.config.labels.ignore = request.config.labels.ignore || [];
-        request.config.labels.reopen = request.config.labels.reopen || [];
-        const contributors = new Set((yield context.github.repos.getContributors(context.repo())).data.map((contributor) => contributor.login));
-        request.isContributor = contributors.has(context.payload.sender.login);
-        request.issue = Object.assign({}, context.payload.issue, { labels: context.payload.issue.labels.map((label) => label.name) });
-        request.edits = context.issue({ state: request.issue.state, labels: [...request.issue.labels] });
-        robot.log(`contributors: ${context.payload.sender.login} / ${Array.from(contributors)} => ${request.isContributor}`);
-        request.ignore = (_.intersection(request.issue.labels, request.config.labels.ignore).length !== 0);
-        request.reopen = (_.intersection(request.issue.labels.concat('*'), request.config.labels.reopen).length !== 0);
-        request.label = name => {
-            if (request.edits.labels.includes(name))
-                return;
-            request.edits.labels.push(name);
-        };
-        request.unlabel = name => {
-            request.edits.labels = request.edits.labels.filter(label => label !== name);
-        };
-        request.save = (reason) => __awaiter(this, void 0, void 0, function* () {
-            if (request.edits.state === request.issue.state && _.isEqual(new Set(request.edits.labels), new Set(request.issue.labels)))
-                return;
-            robot.log(`${reason}(${context.payload.sender.login}${request.isContributor ? '*' : ''}): ${request.issue.state}[${request.issue.labels}] -> ${request.edits.state}[${request.edits.labels}]`);
-            yield context.github.issues.edit(request.edits);
+class ProbotRequest {
+    constructor(robot, context) {
+        this.robot = robot;
+        this.context = context;
+    }
+    load() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // load config
+            let config;
+            try {
+                config = yield this.context.config('config.yml');
+                if (config)
+                    config = config['label-gun'];
+                if (config && config.reopen === '*')
+                    config.reopen = ['*'];
+            }
+            catch (err) {
+                this.robot.log(err);
+                config = null;
+            }
+            this.config = config || { ignore: [], reopen: [], feedback: '' };
+            this.config.feedback = this.config.feedback || 'awaiting-user-feedback';
+            this.config.ignore = this.config.ignore || [];
+            this.config.reopen = this.config.reopen || [];
+            // called by contributor?
+            const contributors = new Set((yield this.context.github.repos.getContributors(this.context.repo())).data.map((contributor) => contributor.login));
+            this.isContributor = contributors.has(this.context.payload.sender.login);
+            // remember state
+            this.issue = Object.assign({}, this.context.payload.issue, { labels: this.context.payload.issue.labels.map((label) => label.name) });
+            this.state = this.issue.state;
+            this.labels = [...this.issue.labels];
+            this.ignore = (_.intersection(this.issue.labels, this.config.ignore).length !== 0);
+            this.reopen = (_.intersection(this.issue.labels.concat('*'), this.config.reopen).length !== 0);
+            return this;
         });
-        return request;
-    });
+    }
+    label(name) {
+        if (this.labels.includes(name))
+            return;
+        this.labels.push(name);
+    }
+    unlabel(name) {
+        this.labels = this.labels.filter(label => label !== name);
+    }
+    save(reason) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (this.state === this.issue.state && _.isEqual(new Set(this.labels), new Set(this.issue.labels)))
+                return;
+            this.robot.log(`${reason}(${this.context.payload.sender.login}${this.isContributor ? '*' : ''}): ${this.issue.state}[${this.issue.labels}] -> ${this.state}[${this.labels}]`);
+            yield this.context.github.issues.edit(this.context.issue({ state: this.state, labels: this.labels }));
+        });
+    }
 }
 module.exports = (robot) => __awaiter(this, void 0, void 0, function* () {
     // Your code here
@@ -66,35 +68,35 @@ module.exports = (robot) => __awaiter(this, void 0, void 0, function* () {
     // To get your app running against GitHub, see:
     // https://probot.github.io/docs/development/
     robot.on('issue_comment.created', (context) => __awaiter(this, void 0, void 0, function* () {
-        const req = yield load(robot, context);
+        const req = yield (new ProbotRequest(robot, context)).load();
         // if a non-contrib comments on a closed issue, re-open it
-        if (req.edits.state === 'closed' && !req.isContributor)
-            req.edits.state = 'open';
-        if (!req.ignore && req.edits.state === 'open') {
+        if (req.state === 'closed' && !req.isContributor)
+            req.state = 'open';
+        if (!req.ignore && req.state === 'open') {
             if (req.isContributor) {
-                req.label(req.config.labels.feedback);
+                req.label(req.config.feedback);
             }
             else {
-                req.unlabel(req.config.labels.feedback);
+                req.unlabel(req.config.feedback);
             }
         }
         yield req.save('issue_comment.created');
     }));
     robot.on('issues.closed', (context) => __awaiter(this, void 0, void 0, function* () {
-        const req = yield load(robot, context);
+        const req = yield (new ProbotRequest(robot, context)).load();
         if (req.isContributor) {
-            req.unlabel(req.config.labels.feedback);
+            req.unlabel(req.config.feedback);
         }
         else if (req.reopen) {
             // if a non-contrib close an issue, re-open it
-            req.edits.state = 'open';
+            req.state = 'open';
         }
         yield req.save('issues.closed');
     }));
     robot.on('issues.reopened', (context) => __awaiter(this, void 0, void 0, function* () {
-        const req = yield load(robot, context);
+        const req = yield (new ProbotRequest(robot, context)).load();
         if (req.isContributor && !req.ignore)
-            req.label(req.config.labels.feedback);
+            req.label(req.config.feedback);
         yield req.save('issues.reopend');
     }));
 });
--- index.ts
+++ index.ts
@@ -1,73 +1,74 @@
 const _ = require('lodash')
 
-async function load(robot: any, context: any) {
-  const request: {
-    config: any
-    isContributor: boolean
+class ProbotRequest {
+  private robot: any
+  private context: any
+  private issue: any
 
-    ignore: boolean
-    reopen: boolean
+  public isContributor: boolean
+  public reopen: boolean
+  public ignore: boolean
 
-    issue: { state: string, labels: Array<string> }
-    edits: { state: string, labels: Array<string> }
+  public state: string
+  public labels: Array<string>
 
-    label: (name: string) => void
-    unlabel: (name: string) => void
-    save: (method: string) => void
-  } = {
-    config: null,
-    isContributor: false,
-    ignore: false,
-    reopen: false,
-
-    issue: null,
-    edits: null,
-
-    label: null,
-    unlabel: null,
-    save: null,
+  public config: {
+    feedback: string
+    ignore: Array<string>
+    reopen: Array<string>
   }
 
-  try {
-    request.config = await context.config('config.yml')
-  } catch (err) {
-    robot.log(err)
-    request.config = null
-  }
-  request.config = ((request.config || {})['label-gun']) || {}
-
-  if (!request.config.labels) request.config.labels = {}
-  if (!request.config.labels.feedback) request.config.labels.feedback = 'awaiting-user-feedback'
-  request.config.labels.ignore = request.config.labels.ignore || []
-  request.config.labels.reopen = request.config.labels.reopen || []
-
-  const contributors = new Set((await context.github.repos.getContributors(context.repo())).data.map((contributor: any) => contributor.login))
-  request.isContributor = contributors.has(context.payload.sender.login)
-  request.issue = { ...context.payload.issue, labels: context.payload.issue.labels.map((label: any) => label.name) }
-  request.edits = context.issue({ state: request.issue.state, labels: [...request.issue.labels] })
-
-  robot.log(`contributors: ${context.payload.sender.login} / ${Array.from(contributors)} => ${request.isContributor}`)
-
-  request.ignore = (_.intersection(request.issue.labels, request.config.labels.ignore).length !== 0)
-  request.reopen = (_.intersection(request.issue.labels.concat('*'), request.config.labels.reopen).length !== 0)
-
-  request.label = name => {
-    if (request.edits.labels.includes(name)) return
-    request.edits.labels.push(name)
+  constructor(robot: any, context: any) {
+    this.robot = robot
+    this.context = context
   }
 
-  request.unlabel = name => {
-    request.edits.labels = request.edits.labels.filter(label => label !== name)
+  public async load() {
+    // load config
+    let config
+    try {
+      config = await this.context.config('config.yml')
+      if (config) config = config['label-gun']
+      if (config && config.reopen === '*') config.reopen = ['*']
+    } catch (err) {
+      this.robot.log(err)
+      config = null
+    }
+    this.config = config || { ignore: [], reopen: [], feedback: '' }
+    this.config.feedback = this.config.feedback || 'awaiting-user-feedback'
+    this.config.ignore = this.config.ignore || []
+    this.config.reopen = this.config.reopen || []
+
+    // called by contributor?
+    const contributors = new Set((await this.context.github.repos.getContributors(this.context.repo())).data.map((contributor: any) => contributor.login))
+    this.isContributor = contributors.has(this.context.payload.sender.login)
+
+    // remember state
+    this.issue = { ...this.context.payload.issue, labels: this.context.payload.issue.labels.map((label: any) => label.name) }
+    this.state = this.issue.state
+    this.labels = [...this.issue.labels]
+
+    this.ignore = (_.intersection(this.issue.labels, this.config.ignore).length !== 0)
+    this.reopen = (_.intersection(this.issue.labels.concat('*'), this.config.reopen).length !== 0)
+
+    return this
   }
 
-  request.save = async reason => {
-    if (request.edits.state === request.issue.state && _.isEqual(new Set(request.edits.labels), new Set(request.issue.labels))) return
-
-    robot.log(`${reason}(${context.payload.sender.login}${request.isContributor ? '*' : ''}): ${request.issue.state}[${request.issue.labels}] -> ${request.edits.state}[${request.edits.labels}]`)
-    await context.github.issues.edit(request.edits)
+  public label(name: string) {
+    if (this.labels.includes(name)) return
+    this.labels.push(name)
   }
 
-  return request
+  public unlabel(name: string) {
+    this.labels = this.labels.filter(label => label !== name)
+  }
+
+  public async save(reason: string) {
+    if (this.state === this.issue.state && _.isEqual(new Set(this.labels), new Set(this.issue.labels))) return
+
+    this.robot.log(`${reason}(${this.context.payload.sender.login}${this.isContributor ? '*' : ''}): ${this.issue.state}[${this.issue.labels}] -> ${this.state}[${this.labels}]`)
+    await this.context.github.issues.edit(this.context.issue({ state: this.state, labels: this.labels }))
+  }
 }
 
 module.exports = async (robot: any) => {
@@ -81,16 +82,16 @@ module.exports = async (robot: any) => {
   // https://probot.github.io/docs/development/
 
   robot.on('issue_comment.created', async (context: any) => {
-    const req = await load(robot, context)
+    const req = await (new ProbotRequest(robot, context)).load()
 
     // if a non-contrib comments on a closed issue, re-open it
-    if (req.edits.state === 'closed' && !req.isContributor) req.edits.state = 'open'
+    if (req.state === 'closed' && !req.isContributor) req.state = 'open'
 
-    if (!req.ignore && req.edits.state === 'open') {
+    if (!req.ignore && req.state === 'open') {
       if (req.isContributor) {
-        req.label(req.config.labels.feedback)
+        req.label(req.config.feedback)
       } else {
-        req.unlabel(req.config.labels.feedback)
+        req.unlabel(req.config.feedback)
       }
     }
 
@@ -98,22 +99,22 @@ module.exports = async (robot: any) => {
   })
 
   robot.on('issues.closed', async (context: any) => {
-    const req = await load(robot, context)
+    const req = await (new ProbotRequest(robot, context)).load()
 
     if (req.isContributor) {
-      req.unlabel(req.config.labels.feedback)
+      req.unlabel(req.config.feedback)
     } else if (req.reopen) {
       // if a non-contrib close an issue, re-open it
-      req.edits.state = 'open'
+      req.state = 'open'
     }
 
     await req.save('issues.closed')
   })
 
   robot.on('issues.reopened', async (context: any) => {
-    const req = await load(robot, context)
+    const req = await (new ProbotRequest(robot, context)).load()
 
-    if (req.isContributor && !req.ignore) req.label(req.config.labels.feedback)
+    if (req.isContributor && !req.ignore) req.label(req.config.feedback)
 
     await req.save('issues.reopend')
   })
